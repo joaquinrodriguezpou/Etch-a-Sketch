6caedf6d2272017d7aac22753cfb295079adae99
Joaquin <138231434+joaquinrodriguezpou@users.noreply.github.com>
2023-09-06 18:23:55 -0300

    add a bar to manipulate number of squares per side

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 const container = document.querySelector('.container');
+const gridSize = document.querySelector('gridSize')
+const slider = document.getElementById('slider');
+const sliderValue = document.getElementById('sliderValue');
+let value = 16;
 
-for (let i = 0; i < (16*16); i++) {
-    const div = document.createElement('div');
+for (let i = 0; i < (value**2); i++) {
+   const div = document.createElement('div');
     div.classList.add('square');
     container.appendChild(div);
 }
 
 const squares = Array.from(document.querySelectorAll('.square'));
+function getRandomNumber(){
+    return Math.floor(Math.random() * 250);
+}
 
 squares.forEach(div => div.addEventListener('mouseover', function(){
-    //let randomColor = `${getRandomNumber}, ${getRandomNumber}, ${getRandomNumber}`;
-    function getRandomNumber(){
-        return Math.floor(Math.random() * 250);
-    }
     const randomColor = `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`;
     this.style.backgroundColor = randomColor;
 }
 ))
+
+
+slider.addEventListener('input', function() {
+    value = this.value;
+    sliderValue.textContent = value + 'x' + value; 
+    container.innerHTML = '';
+    const squareSize = 100 / value + '%';
+    for (let i = 0; i < (value**2); i++) {
+        const div = document.createElement('div');
+        div.classList.add('square');
+        container.appendChild(div);
+        div.style.width = squareSize;
+    };
+});
